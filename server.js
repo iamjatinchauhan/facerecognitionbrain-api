895c18940dbd318c786965d015b00da9da159edf
mrjatinchauhan <jatin.405chauhan@gmail.com>
2020-05-19 23:57:58 +0530

    adding PORT

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req,res) => {
-    res.send(database.users);
+    res.send('it is working!');
 })
 
 app.post('/Signin', signin.handleSignin(db, bcrypt))
