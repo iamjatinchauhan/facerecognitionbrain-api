5d8477ccabd1bbbe69f232f538e8e1ccd121d1c4
mrjatinchauhan <jatin.405chauhan@gmail.com>
2020-05-20 16:43:56 +0530

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-transparent-84949',
       user : 'postgres',
       password : 'admin',
       database : 'facerecognitionbraindatabase'
