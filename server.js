799be02931f46be80aeef65f825915c95eb74d31
mrjatinchauhan <jatin.405chauhan@gmail.com>
2020-05-20 16:53:16 +0530

    adding database connection fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false
       }
